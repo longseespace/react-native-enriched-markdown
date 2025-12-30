5b1a214436634680db24c18125d96d57f6b9e58c
Gregory Moskaliuk <mosckalyuck@gmail.com>
2025-12-30 23:28:33 +0100

    refactor(android): simplify marginBottom calculation in HeadingStyle

--- android/src/main/java/com/richtext/styles/HeadingStyle.kt
+++ android/src/main/java/com/richtext/styles/HeadingStyle.kt
@@ -23,9 +23,7 @@ data class HeadingStyle(
       val fontFamily = parser.parseString(map, "fontFamily")
       val fontWeight = parser.parseString(map, "fontWeight", "normal")
       val color = parser.parseColor(map, "color")
-      // Default marginBottom: h1=0, h2-h6=24
-      val defaultMarginBottom = if (level == 1) 0.0 else 24.0
-      val marginBottom = parser.toPixelFromDIP(parser.parseOptionalDouble(map, "marginBottom", defaultMarginBottom).toFloat())
+      val marginBottom = parser.toPixelFromDIP(parser.parseOptionalDouble(map, "marginBottom", 24.0).toFloat())
       val lineHeightRaw = parser.parseOptionalDouble(map, "lineHeight", 0.0).toFloat()
       val lineHeight = parser.toPixelFromSP(lineHeightRaw)
 
